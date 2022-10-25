fbb869cf3ae1241f9fd2dd3c5b9986aa3dbdacde
Luke Montgomery <lumontgomery@westmont.edu>
2022-10-25 15:54:31 -0700

    finish script

--- runme.sh
+++ runme.sh
@@ -1,15 +1,15 @@
 #!bin/bash
 if ./dlc bits.c; then
   echo "dlc tests pass!"
-  echo "making btests..."
+  echo "making..."
   make clean
+  make
   make btest
   echo ""
   ./btest
-
 else
   echo "Failed dlc test:"
   ./dlc -e bits.c
-  exit 1
+  exit 0
 fi
 exit 0
