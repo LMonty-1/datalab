0516d64e00adef765755d444b2cba80ff13721d4
Luke Montgomery <lumontgomery@westmont.edu>
2022-10-27 16:53:58 -0700

    j

--- bits.c
+++ bits.c
@@ -756,10 +756,10 @@ int real_to_int(unsigned r) {
 
     return myEx;
 
-    if(myEx == 0 ) { /* Exactly 0  or Denormalized */
+    if(myEx < 0 ) { /* Exactly 0  or Denormalized */
         return 0; 
     }
-    if(myEx == 255) { /* Special, Infinity or NaN */
+    if(myEx >= 255) { /* Special, Infinity or NaN */
         return 0x80000000u;
     }
 
