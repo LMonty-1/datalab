156d0d2d1f870c2bfd9dcac3b9f89f067a03bf43
Luke Montgomery <lumontgomery@westmont.edu>
2022-10-27 20:15:49 -0700

    p

--- bits.c
+++ bits.c
@@ -756,11 +756,14 @@ int real_to_int(unsigned r) {
 
     // return myEx;
 
-    if(myEx < 0 || myFrac == 0) { // Exactly 0  or Denormalized
+    if (myEx < 0) { // Exactly 0  or Denormalized
         return 0; 
     }
-     if(myEx >= 255 && myFrac != 0) { // Special, Infinity or NaN
+    if (myEx >= 255) { // Special, Infinity or NaN
         return 0x80000000u;
+    }
+    if (myFrac == 0) {
+        return 1;
     }
 
     for (i = 0; i < myEx-1; i++) {
